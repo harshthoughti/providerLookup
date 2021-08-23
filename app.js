5220740a032b35877a0bf79bd09dc9b699ca227c
harshthoughti <89014357+harshthoughti@users.noreply.github.com>
2021-08-23 14:59:43 +0530

    Updating app.js and ejs file

--- app.js
+++ app.js
@@ -9,54 +9,42 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 const port = 3000
 
-
 mongoose.connect('mongodb://localhost:27017/MyDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const userSchema = new mongoose.Schema({
-
   Organisation : String,
   firstName : String,
   lastName : String,
   npiNumber : Number
-
 });
 
 const Lookup = mongoose.model("provider", userSchema)
 
 const provider1 = new Lookup({
-
   Organisation : "HIMERX PHARMACY LLC",
   npiNumber : 1568912004
-
 });
 provider1.save();
 
 const provider2 = new Lookup({
-
   firstName : "GASTON",
   lastName : "BIANCA",
   npiNumber : 1477124121
-
 });
 provider2.save();
 
 const provider3 = new Lookup({
-
   firstName : "SMITH",
   lastName : "LISA",
   npiNumber : 1801069596
-
 });
 provider3.save();
 
 const provider4 = new Lookup({
-
   Organisation : "EMERALD HILLS HAND THERAPY LLC",
   npiNumber : 1144852872
-
 });
 provider4.save();
-
 
 app.get('/providerlookup', (req, res) => {
   res.sendFile(__dirname + "/lookup.html")
@@ -64,43 +52,99 @@ app.get('/providerlookup', (req, res) => {
 
 app.post('/providerlookup', (req, res) => {
   if (req.body.n1) {
-    Lookup.find( {firstName : req.body.n1} , (function(err, result_list){
+    if (req.body.n2) {
+      Lookup.find( {firstName : req.body.n1, lastName : req.body.n2} , (function(err, result_list){
 
-    if(result_list){
-      res.render('table', {'result' : result_list});
-    }
-    else{
-      res.send("No Data Found");
-    }
-  }))
-  }
-  else if (req.body.n2) {
-    Lookup.find( {lastName : req.body.n2} , function(err, result_list){
+        if(result_list){
+          res.render('table', {'result' : result_list});
+        }
+      }))
+    } else if (req.body.n3) {
+      Lookup.find( {firstName : req.body.n1, npiNumber : req.body.n3} , (function(err, result_list){
 
-      if(result_list){
-        res.render('table', {'result' : result_list});
-    }
-    else{
-      res.send("No Data Found");
-    }
-  })
-  }
-  else if (req.body.n3) {
-    Lookup.find( {npiNumber : req.body.n3 }, function(err, result_list){
+        if(result_list){
+          res.render('table', {'result' : result_list});
+        }
+      }))
+    } else {
+      Lookup.find( {firstName : req.body.n1} , (function(err, result_list)
+      {
+        if(result_list)
+        {
+          res.render('table', {'result' : result_list});
+        }
+      }))
+    }}
+    else if (req.body.n2) {
+      if (req.body.n1) {
+        Lookup.find( {firstName : req.body.n1, lastName : req.body.n2} , (function(err, result_list){
 
-      if(result_list){
-        res.render('table', {'result' : result_list});
-    }
-    else{
-      res.send("No Data Found");
-    }
-  })
-  }
-  else{
-    res.send("No Data Found")
-  }
-})
+          if(result_list){
+            res.render('table', {'result' : result_list});
+          }
+          else{
+            res.send("No Data Found");
+          }
+        }))
+      } else if (req.body.n3) {
+        Lookup.find( {lastName : req.body.n2, npiNumber : req.body.n3} , (function(err, result_list){
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
-})
+          if(result_list){
+            res.render('table', {'result' : result_list});
+          }
+          else{
+            res.send("No Data Found");
+          }
+        }))
+      } else {
+        Lookup.find( {lastName : req.body.n2} , (function(err, result_list){
+
+          if(result_list){
+            res.render('table', {'result' : result_list});
+          }
+          else{
+            res.send("No Data Found");
+          }
+        }))
+      }
+    }
+    else if (req.body.n3) {
+      if (req.body.n1) {
+        Lookup.find( {firstName : req.body.n1, npiNumber : req.body.n3} , (function(err, result_list){
+
+          if(result_list){
+            res.render('table', {'result' : result_list});
+          }
+          else{
+            res.send("No Data Found");
+          }
+        }))
+      } else if (req.body.n2) {
+        Lookup.find( {lastName : req.body.n2, npiNumber : req.body.n3} , (function(err, result_list){
+
+          if(result_list){
+            res.render('table', {'result' : result_list});
+          }
+          else{
+            res.send("No Data Found");
+          }
+        }))
+      } else {
+        Lookup.find( {npiNumber : req.body.n3} , (function(err, result_list){
+
+          if(result_list){
+            res.render('table', {'result' : result_list});
+          }
+          else{
+            res.send("No Data Found");
+          }
+        }))
+      }}
+
+      else{
+        res.send("No Data Found");
+      }
+    })
+    app.listen(port, () => {
+      console.log(`Listening at http://localhost:${port}`)
+      })
